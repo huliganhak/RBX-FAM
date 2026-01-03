7df010e4dff122300f39f75d7866cac731756409
huliganhak <133084905+huliganhak@users.noreply.github.com>
2026-01-03 21:11:27 +0700

    Update 004.lua

--- 004.lua
+++ 004.lua
@@ -23,7 +23,7 @@ gui.ResetOnSpawn = false
 gui.Parent = player:WaitForChild("PlayerGui")
 
 local frame = Instance.new("Frame")
-frame.Size = UDim2.new(0, 400, 0, 320)
+frame.Size = UDim2.new(0, 450, 0, 300)
 frame.AnchorPoint = Vector2.new(0.5, 0.5)
 frame.Position = UDim2.new(0.5, 0, 0.5, 0)
 frame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
@@ -463,5 +463,6 @@ stopBtn.MouseButton1Click:Connect(function()
 	running = false
 	setStatus("Stopping...")
 end)
+
 
 
